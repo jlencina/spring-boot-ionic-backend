3ab0718556c2e04b947345263d601d80f80b9172
Juan Lencina <juan.lencina@hotmail.com>
2018-10-11 13:49:56 -0300

    Estado e cidade

---
+++ src/main/java/com/juanti/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.juanti.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.juanti.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	
+}
