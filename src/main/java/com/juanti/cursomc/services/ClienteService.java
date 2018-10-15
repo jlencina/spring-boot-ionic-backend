239aacc2734ab611bc3aa9806f169ca1bc9e8e0e
Juan Lencina <juan.lencina@hotmail.com>
2018-10-15 14:24:19 -0300

    Endpoint /clientes/{id} disponível

--- src/main/java/com/juanti/cursomc/services/ClienteService.java
+++ src/main/java/com/juanti/cursomc/services/ClienteService.java
@@ -5,19 +5,19 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.juanti.cursomc.domain.Categoria;
-import com.juanti.cursomc.repositories.CategoriaRepository;
+import com.juanti.cursomc.domain.Cliente;
+import com.juanti.cursomc.repositories.ClienteRepository;
 import com.juanti.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class CategoriaService {
+public class ClienteService {
 	
 	@Autowired
-	private CategoriaRepository repo;
+	private ClienteRepository repo;
 	
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
 }
